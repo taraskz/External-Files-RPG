482920d1ba58b298c043b4811b0765b23335af2c
taraskz <158589585+taraskz@users.noreply.github.com>
2024-03-26 14:33:08 +0000

    made a map file that prints a map of the rooms

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ a long red carpet.",
   "Kitchen": "Now you're in the kitchen of the castle, it smells great",
   "Kings Room": "You are in the kings room where he sleeps, hes not here",
   "Dinning Room":"Now you're in the dinning room where the king eats",
-  "Throne Room":"You ended up in the throne room where the king sits"
+  "Throne Room":"You ended up in the throne room where the king sits",
+  "Bathroom": "You are in the bathroom where the king does his buisness"
       }
 
 room_location = {
@@ -16,10 +17,36 @@ room_location = {
   "Dinning Room": (0,-2), # two down
   "Throne Room": (1,0), # one right
   "Bathroom": (2,0) # two right
+}
+tile = ["Main Hall", "Kitchen",
+        "Kings Room","Dinning Room",
+        "Throne Room","Bathroom",
+        "Hallway"
+       ]
 
+
+tiles = [
+       [tile[0],tile[4],tile[5]],
+       [tile[1],tile[2],tile[6]],
+       [tile[3],tile[6],tile[6]]
+       ]
 # external file name 
-mapfile = "map.txt"
+mapfile = 'map.txt'
 
 
-def read_map():
+def export_map():
+    try:
+        with open(mapfile, 'w') as file:
+            file.write(tabulate(tiles, tablefmt = "fancy_grid"))
+    except: 
+        print("Somethinf went wrong")
+    else: 
+        print("Here is the map to the castle")
+    finally:
+        print("Good luck!")
+
+
+
+export_map()
+
     
